7b19753bcd4a00a6157e3ff42e432be03df7c881
Namemai <40319864+Namemai@users.noreply.github.com>
2020-06-11 23:09:39 +0700

    Update 5.py

--- 5.py
+++ 5.py
@@ -164,19 +164,19 @@ def kick(grup, target):
 
 def cancel(grup, target):
     try:
-        ki.cancelGroupInvitation(grup, [op.param2])
+        ki.cancelGroupInvitation(grup, [target])
     except:
         try:
-            kk.cancelGroupInvitation(grup, [op.param2])
+            kk.cancelGroupInvitation(grup, [target])
         except:
             try:
-                kc.cancelGroupInvitation(grup, [op.param2])
+                kc.cancelGroupInvitation(grup, [target])
             except:
                 try:
-                    km.cancelGroupInvitation(grup, [op.param2])
+                    km.cancelGroupInvitation(grup, [target])
                 except:
                     try:
-                        k5.cancelGroupInvitation(grup, [op.param2])
+                        k5.cancelGroupInvitation(grup, [target])
                     except:
                         pass
 
@@ -1095,7 +1095,7 @@ async def cerberusRun():
 								t9 = threading.Thread(target=blacklist, args=(op.param2,)).start()
 								try:
 									t10 = threading.Thread(target=kick, args=(op.param1, op.param2)).start()
-									t11 = threading.Thread(target=cancel, args=(op.param1, op.param2)).start()
+									t11 = threading.Thread(target=cancel, args=(op.param1, op.param3)).start()
 								except:
 									pass
 						if op.param2 in status["blacklist"]:
@@ -1105,7 +1105,7 @@ async def cerberusRun():
 								t12 = threading.Thread(target=blacklist, args=(op.param2,)).start()
 								try:
 									t13 = threading.Thread(target=kick, args=(op.param1, op.param2)).start()
-									t14 = threading.Thread(target=cancel, args=(op.param1, op.param2)).start()
+									t14 = threading.Thread(target=cancel, args=(op.param1, op.param3)).start()
 								except:
 									pass
 						if op.param3 in status["blacklist"]:
@@ -1115,7 +1115,7 @@ async def cerberusRun():
 								t15 = threading.Thread(target=blacklist, args=(op.param2,)).start()
 								try:
 									t16 = threading.Thread(target=kick, args=(op.param1, op.param2)).start()
-									t17 = threading.Thread(target=cancel, args=(op.param1, op.param2)).start()
+									t17 = threading.Thread(target=cancel, args=(op.param1, op.param3)).start()
 								except:
 									pass
 						if mid in op.param3:
